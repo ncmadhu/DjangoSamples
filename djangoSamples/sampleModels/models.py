ab305d52539a63fa39ba02a63fe5b3c6464125fd
Madhu Chakrvarthy <ncmadhu@gmail.com>
2017-12-17 13:13:41 -0800

    Added sample for many to many relationship

--- djangoSamples/sampleModels/models.py
+++ djangoSamples/sampleModels/models.py
@@ -68,3 +68,65 @@ class UserProfile(models.Model):
     def __str__(self):
         return "{0} {1}".format(self.favourite_sport, self.favourite_color)
 
+class ModelYear(models.Model):
+    """
+    Example for many to many relationship field
+    In many to many relationship one model is related to many other models
+    In this example in a model year several cars could have been manufactured
+    and the same type of car model could have been manufactured in several years
+        ----------------                                                 ----------------
+        |              |------------------ Car Model 1 ------------------|              |
+        | Model year 1 |                   Car Model 2 ------------------| Model year 2 |
+        |              |------------------ Car Model 3                   |              |
+        ----------------                                                 ----------------
+    In tha above example in Model year 1 car model 1 and 3 are manufactured
+    and car model 1 is also manufactured in Model year 2
+    """
+    model_year = models.IntegerField(max_length=4, help_text="Model Year")
+
+    def __str__(self):
+        return str(self.model_year)
+
+class Car(models.Model):
+    """
+    Example for many to many relationship field
+    In many to many relationship one model is related to many other models
+    eg.
+    model_year84 = ModelYear(model_year=1984)
+    model_year84.save()
+    model_year85 = ModelYear(model_year=1984)
+    model_year85.save()
+    car = Car(model_name="Punto", manufacturer_name="Fiat")
+    car.save()
+    car.model_year.add(model_year84)
+    car.model_year.add(model_year85)
+    car.save()
+    anothercar = Car(model_name="Fusion", manufacturer_name="Ford")
+    anothercar.save()
+    anothercar.model_year.add(model_year85)
+    anothercar.save()
+
+    DB SAMPLE OUTPUT:
+
+    djangosamples=> SELECT * FROM "sampleModels_car";
+      1 | Punto      | Fiat
+      2 | Fusion     | Ford
+
+    djangosamples=> SELECT * FROM "sampleModels_modelyear";
+      2 |       1984
+      3 |       1985
+
+    djangosamples=> SELECT * FROM "sampleModels_car_model_year";
+      1 |      1 |            2
+      2 |      1 |            3
+      3 |      2 |            3
+
+    djangosamples=>
+    """
+
+    model_name = models.CharField(max_length=10, help_text="Model Name")
+    manufacturer_name = models.CharField(max_length=256, help_text="Car Manufacturer")
+    model_year = models.ManyToManyField(ModelYear, help_text="Model Year")
+
+    def __str__(self):
+        return "{0} {1}".format(self.manufacturer_name, self.model_name)
